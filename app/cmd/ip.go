c3f79d3a58dbcb457b8c8d28fcda1a2a512fcdf5
jakub <jakub.dalek@utoronto.ca>
2021-05-16 10:04:23 -0400

    help msg change

--- app/cmd/ip.go
+++ app/cmd/ip.go
@@ -9,9 +9,9 @@ var resolveDomains = false
 var showIntel = false
 
 var rootCmd = &cobra.Command{
-	Use:   "ip <any ip>",
-	Short: "get info on a single ip",
-	Long:  "get information on a single ip",
+	Use:   "ipd <any ip>",
+	Short: "Get GeoIP info on a single ip",
+	Long:  "Get GeoIP info on a single ip",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		LookupOne(args[0])
